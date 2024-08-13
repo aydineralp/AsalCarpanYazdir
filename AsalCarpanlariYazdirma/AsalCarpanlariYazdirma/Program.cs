a7db1b9cc233184e3f8852a93628db132894c21e
Veysel Aydın ERALP <veyselaydineralp@gmail.com>
2024-08-13 10:33:19 +0300

    Asal Çarpanları Yazdır

---
+++ AsalCarpanlariYazdirma/AsalCarpanlariYazdirma/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AsalCarpanlariYazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            List<int> asalCarpanlar = AsalCarpanlar(sayi);
+
+            if (asalCarpanlar.Count == 0)
+            {
+                Console.WriteLine("Girilen sayı 1 veya negatif olduğu için asal çarpanı yoktur.");
+            }
+            else
+            {
+                Console.WriteLine($"{sayi} sayısının asal çarpanları: {string.Join(", ", asalCarpanlar)}");
+            }
+        }
+
+        static List<int> AsalCarpanlar(int sayi)
+        {
+            List<int> carpanlar = new List<int>();
+
+            for (int i = 2; i <= sayi; i++)
+            {
+                while (sayi % i == 0)
+                {
+                    carpanlar.Add(i);
+                    sayi /= i;
+                }
+            }
+
+            return carpanlar;
+        }
+    }
+}
